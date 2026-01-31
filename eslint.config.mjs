9522fbe52a0d905cf646dfd00f58fc4491d409e2
Molinos <cabrera.molinos@gmail.com>
2026-01-31 17:45:45 +0000

    estructura y eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,6 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import { defineConfig, globalIgnores } from 'eslint/config'
+import nextVitals from 'eslint-config-next/core-web-vitals'
+import nextTs from 'eslint-config-next/typescript'
 
 const eslintConfig = defineConfig([
   ...nextVitals,
@@ -8,11 +8,24 @@ const eslintConfig = defineConfig([
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
+    '.next/**',
+    'out/**',
+    'build/**',
+    'next-env.d.ts'
   ]),
-]);
+  {
+    rules: {
+      quotes: ['error', 'single'], // usa comillas simples
+      semi: ['error', 'never'], // sin punto y coma
+      'eol-last': ['error', 'always'], // nueva línea al final del archivo
+      indent: ['error', 2], // indentación de 2 espacios
+      'comma-dangle': ['error', 'never'], // sin coma al final
+      'no-unused-vars': ['warn', { // advertencia para variables no usadas
+        argsIgnorePattern: '^_', // ignora parámetros que empiezan con _
+        varsIgnorePattern: '^_' // ignora variables que empiezan con _
+      }]
+    }
+  }
+])
 
-export default eslintConfig;
+export default eslintConfig
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   /* config options here */
-};
+  allowedDevOrigins: ['*.cluster-hlmk2l2htragyudeyf6f3tzsi6.cloudworkstations.dev']
+}
 
-export default nextConfig;
+export default nextConfig
